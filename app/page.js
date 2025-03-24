01a62cd75ef1f9e48caf3e6330e2293041244510
3dguruai <barbara.villanova@students.iaac.net>
2025-03-23 21:59:35 -0300

    b

--- app/page.js
+++ app/page.js
@@ -5,12 +5,12 @@ export default function Home() {
    <div className="w-full h-screen relative">
     <div className="w-full h-full z-10">
       Sou uma TV velha
-      <Image src="/frame2.png" alt="frame" fill className="object-cover"/>
+      {/* <Image src="/frame2.png" alt="frame" fill className="object-cover"/> */}
     </div>
     <div className="absolute inset-0 flex items-center justify-center">
-      <video autoPlay loop muted playsInline className="w-[96%] ">
+      {/* <video autoPlay loop muted playsInline className="w-[96%] ">
         <source src="/frame--d0.mp4" type="video/mp4"/>
-      </video>
+      </video> */}
     </div>
    </div>
   );
