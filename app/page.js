19f3699f39ebb0264989ced28ea9e912d1138e36
3dguruai <barbara.villanova@students.iaac.net>
2025-03-23 21:43:02 -0300

    frame

--- app/page.js
+++ app/page.js
@@ -2,10 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="w-full h-screen">
-   <video autoPlay loop muted playsInline className="w-full">
-      <source src="/frame--d0.mp4" type="video/mp4"/>
-   </video>
+   <div className="w-full h-screen relative">
+    <div className="w-full h-full z-10">
+      <Image src="/frame2.png" alt="frame" fill className="object-cover"/>
+    </div>
+    <div className="absolute inset-0 flex items-center justify-center">
+      <video autoPlay loop muted playsInline className="w-[96%] ">
+        <source src="/frame--d0.mp4" type="video/mp4"/>
+      </video>
+    </div>
    </div>
   );
 }
